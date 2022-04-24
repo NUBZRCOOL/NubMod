0f76fb3c534c281bf7bf3ee2285f8e674286e88e
NUBZRCOOL <bhattinubaid@gmail.com>
2022-04-24 16:01:52 -0700

    Added recipes for meme items. Added sword item

--- src/main/java/com/nubzrcool/nubmod/init/ItemInit.java
+++ src/main/java/com/nubzrcool/nubmod/init/ItemInit.java
@@ -1,9 +1,11 @@
 package com.nubzrcool.nubmod.init;
 
 import com.google.common.base.Supplier;
+import net.minecraft.world.item.crafting.Ingredient;
 import com.nubzrcool.nubmod.NubMod;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -16,6 +18,9 @@ public class ItemInit {
 	
 	//Boring Item
 	public static final RegistryObject<Item> BORING_ITEM = register("boring_item", () -> new Item(new Item.Properties().tab(NubMod.NUB_TAB)));
+	
+	//Amazing sword
+	public static final RegistryObject<SwordItem> AMAZING_SWORD = register("amazing_sword", () -> new SwordItem(new BaseToolMap(0, 1, 1, 1, 1000, () -> Ingredient.of(BORING_ITEM.get())), 24, -3f, new Item.Properties().tab(NubMod.NUB_TAB)));
 	
 	
 	//--------------
